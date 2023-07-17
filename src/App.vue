26e85a43fe3e6a2b6a38e51aeb5fa2f7f5bfece8
andrii <andriinkn@gmail.com>
2023-07-17 13:48:37 +0300

    initial 1.0.0

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+import TheNav from "./components/TheNav.vue";
+</script>
+
+<template>
+  <TheHeader />
+
+  <main class="flex flex-grow flex-col">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dignissimos voluptatum modi, voluptatibus mollitia
+    nesciunt nihil reiciendis corporis sapiente quam blanditiis aliquid dolorem molestiae consectetur doloribus optio,
+    magni reprehenderit laudantium!
+  </main>
+
+  <TheNav />
+</template>
