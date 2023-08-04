b11eb774675576b8e7aac401342045d109ac2a4d
andrii <andriinkn@gmail.com>
2023-08-04 12:54:03 +0300

    added configuration of external properties (props)

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -2,7 +2,17 @@
 import BaseButton from './BaseButton.vue';
 import { XMarkIcon } from '@heroicons/vue/24/outline';
 
-defineProps(['selected', 'options', 'placeholder']);
+defineProps({
+  selected: Number,
+  options: {
+    required: true,
+    type: Array,
+  },
+  placeholder: {
+    required: true,
+    type: String,
+  },
+});
 </script>
 
 <template>
