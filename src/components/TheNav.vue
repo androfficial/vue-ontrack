afbdf18960453c95d9f8de5943587c332059c00d
andrii <andriinkn@gmail.com>
2023-08-04 13:18:39 +0300

    added file with reuse of validators

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { NAV_ITEMS } from '../constants/constants';
+import { isPageValid } from '../validators/validators';
 import NavItem from './NavItem.vue';
 
 defineProps({
   currentPage: {
     required: true,
     type: String,
-    validator(currentPage) {
-      return Object.keys(NAV_ITEMS).includes(currentPage);
-    },
+    validator: isPageValid,
   },
 });
 
