4862eac16c98716f1b985261c2f255a71d746583
andrii <andriinkn@gmail.com>
2023-08-08 12:18:04 +0300

    added object array validators

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,17 +1,14 @@
 <script setup>
-import BaseButton from './BaseButton.vue';
 import { XMarkIcon } from '@heroicons/vue/24/outline';
+import { validateSelectOptions } from '../validators/validators';
+import BaseButton from './BaseButton.vue';
 
 defineProps({
   selected: Number,
   options: {
     required: true,
     type: Array,
-    validator(options) {
-      return options.every(
-        ({ value, label }) => typeof value === 'number' && typeof label === 'string'
-      );
-    },
+    validator: validateSelectOptions,
   },
   placeholder: {
     required: true,
