81932d1ddd74d141f1bbb6a7b48ee0745e951ddc
andrii <andriinkn@gmail.com>
2023-08-08 12:10:08 +0300

    code refactoring

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,10 @@
-import {
-  PAGE_TIMELINE,
-  PAGE_ACTIVITIES,
-  PAGE_PROGRESS,
-  HOURS_IN_DAY,
-} from '../constants/constants';
+import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR } from '../constants/constants';
+import { isPageValid } from '../validators/validators';
 
 export const normalizePageHash = () => {
-  const hash = window.location.hash.slice(1);
+  const page = window.location.hash.slice(1);
 
-  if (Object.keys([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS]).includes(hash)) return hash;
+  if (isPageValid(page)) return page;
   window.location.hash = PAGE_TIMELINE;
   return PAGE_TIMELINE;
 };
@@ -16,7 +12,7 @@ export const normalizePageHash = () => {
 export function generateTimelineItems() {
   const timelineItems = [];
 
-  for (let hour = 0; hour < HOURS_IN_DAY; hour++) {
+  for (let hour = MIDNIGHT_HOUR; hour < HOURS_IN_DAY; hour++) {
     timelineItems.push({ hour });
   }
 
