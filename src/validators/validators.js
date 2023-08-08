bc4b7e7a8a6ff403aeff1a6b08d295ae031cff3d
andrii <andriinkn@gmail.com>
2023-08-08 16:31:10 +0300

    code refactoring

--- src/validators/validators.js
+++ src/validators/validators.js
@@ -4,20 +4,20 @@ export function isPageValid(page) {
   return Object.keys(NAV_ITEMS).includes(page);
 }
 
-export function isHourValid(hour) {
-  return isNumber(hour) && isBetween(hour, MIDNIGHT_HOUR, HOURS_IN_DAY - 1);
+export function validateTimelineItems(timelineItems) {
+  return timelineItems.every(isTimelineItemValid);
 }
 
 export function isTimelineItemValid({ hour }) {
   return isHourValid(hour);
 }
 
-export function validateSelectOptions(options) {
-  return options.every(isSelectOptionValid);
+export function isHourValid(hour) {
+  return isNumber(hour) && isBetween(hour, MIDNIGHT_HOUR, HOURS_IN_DAY - 1);
 }
 
-export function validateTimelineItems(timelineItems) {
-  return timelineItems.every(isTimelineItemValid);
+export function validateSelectOptions(options) {
+  return options.every(isSelectOptionValid);
 }
 
 export function isUndefinedOrNull(value) {
@@ -28,26 +28,26 @@ export function isNumberOrNull(value) {
   return isNumber(value) || isNull(value);
 }
 
-function isUndefined(value) {
-  return value === undefined;
-}
-
-function isNull(value) {
-  return value === null;
-}
-
-function isString(value) {
-  return typeof value === 'string';
-}
-
-function isNumber(value) {
-  return typeof value === 'number';
+function isSelectOptionValid({ value, label }) {
+  return isNumber(value) && isString(label);
 }
 
 function isBetween(value, start, end) {
   return value >= start && value <= end;
 }
 
-function isSelectOptionValid({ value, label }) {
-  return isNumber(value) && isString(label);
+function isNumber(value) {
+  return typeof value === 'number';
+}
+
+function isString(value) {
+  return typeof value === 'string';
+}
+
+function isUndefined(value) {
+  return value === undefined;
+}
+
+function isNull(value) {
+  return value === null;
 }
