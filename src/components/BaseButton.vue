161fdb1972c77c94a0dcb5597c846a2f81365d7c
andrii <andriinkn@gmail.com>
2023-08-09 12:41:25 +0300

    added activity creation form

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,7 +1,12 @@
 <script>
-import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '../constants/constants';
+import {
+  BUTTON_TYPE_PRIMARY,
+  BUTTON_TYPE_NEUTRAL,
+  BUTTON_TYPE_DANGER,
+} from '../constants/constants';
 
 const typeClasses = {
+  [BUTTON_TYPE_PRIMARY]: 'bg-purple-500 enabled:hover:bg-purple-600 text-white',
   [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
   [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
 };
@@ -12,7 +17,7 @@ import { isButtonTypeValid } from '../validators/validators';
 
 defineProps({
   type: {
-    default: BUTTON_TYPE_NEUTRAL,
+    default: BUTTON_TYPE_PRIMARY,
     type: String,
     validator: isButtonTypeValid,
   },
