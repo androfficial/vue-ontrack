aff6bae8e5385db30df8f7a7b7980d757cd8f5f6
andrii <andriinkn@gmail.com>
2023-08-09 13:41:36 +0300

    added fix for activity validation errors

--- src/validators/validators.js
+++ src/validators/validators.js
@@ -20,8 +20,8 @@ export function validateActivities(activities) {
   return activities.every(isActivityValid);
 }
 
-export function isActivityValid(activity) {
-  return isNotEmptyString(activity);
+export function isActivityValid({ id, name, secondsToComplete }) {
+  return [isNotEmptyString(id), isNotEmptyString(name), isNumber(secondsToComplete)].every(Boolean);
 }
 
 function isNotEmptyString(value) {
@@ -49,7 +49,7 @@ export function isNumberOrNull(value) {
 }
 
 function isSelectOptionValid({ value, label }) {
-  return isNumber(value) && isNotEmptyString(label);
+  return (isNumber(value) || isNotEmptyString(value)) && isNotEmptyString(label);
 }
 
 function isBetween(value, start, end) {
